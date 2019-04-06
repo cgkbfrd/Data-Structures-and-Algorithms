5b85824c3737dfd70c533fb55c6daab74add9a7d
Tone Lee <43435819+cgkbfrd@users.noreply.github.com>
2019-04-06 15:56:15 +0800

    Update and rename Fibonacci to climb_Stairs.py

--- Leetcode/Dynamic_plan/climb_Stairs.py
+++ Leetcode/Dynamic_plan/climb_Stairs.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+
+"""方法一【Fibonacci数列】"""
 """解决上楼梯问题【每次走1或2步】"""
 def  Fibonacci(n):
     if n == 1:
@@ -10,3 +12,11 @@ def  Fibonacci(n):
     for i in range(3, n+1):
         v1, v2= v2, v1 + v2
     return v2 
+
+"""方法二【递归分两步走】"""
+def  climb_Stairs(i, n):
+    if i > n:
+        return 0
+    if i == n:
+        return 1
+    return  climb_Stairs(i+1, n) + climb_Stairs(i+2, n)
