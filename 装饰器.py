8ecf756b509c69586806be9ccd21771653ff8786
Tone Lee <43435819+cgkbfrd@users.noreply.github.com>
2019-07-05 17:10:37 +0800

    Create 装饰器.py

---
+++ 装饰器.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+def makeitalic(func):
+    def wrapped(*args, **kwargs):
+        ret = func(*args, **kwargs)
+        return '<i>' + ret + '</i>'
+    return wrapped
+
+@makeitalic
+def hello(name):
+    return 'hello %s' % name
+
+@makeitalic
+def hello2(name1, name2):
+    return 'hello %s, %s' % (name1, name2)
+    
+
+if __name == '__main__':
+    a = hello  #此时返回的是函数wrapped
+    b = hello("Python")  #此时将返回的函数wrapped进一步执行wrapped("Python")，相当于走了两步，b是返回函数wrapped执行的结果
+    
+    
+=====================结果===========================
+>>> hello('python')
+'<i>hello python</i>'
+>>> hello2('python', 'java')
+'<i>hello python, java</i>'
